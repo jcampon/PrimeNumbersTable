32e74147b1c59a2310c4ced58b8072157ac9b4d7
Jesús Campón <jesus.campon@gmail.com>
2016-05-22 18:55:23 +0100

    Added domain repository for handling the logic of generating a list of prime numbers and added some initial unit tests

---
+++ PrimeNumbersTable.Tests/Domain/Repositories/PrimeNumbersRepositoryTest.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Web.Mvc;
+using Moq;
+using NUnit.Framework;
+using PrimeNumbersTable.Web;
+using PrimeNumbersTable.Web.Domain.Repositories;
+
+namespace PrimeNumbersTable.Tests.Domain.Repositories
+{
+    [TestFixture]
+    public class when_working_with_the_PrimeNumbersRepository
+    {
+        protected Mock<IPrimeNumbersRepository> RepositoryMock;
+
+        [SetUp]
+        protected void SetUpRepository()
+        {
+            this.RepositoryMock = new Mock<IPrimeNumbersRepository>();
+        }
+    }
+
+    [TestFixture]
+    public class and_getting_a_list_of_prime_numbers : when_working_with_the_PrimeNumbersRepository
+    {
+        private void SetUpGetListOfPrimeNumbersMethod(int totalOfNumbers)
+        {
+            var listOfNumbers = new int[totalOfNumbers];
+
+            base.RepositoryMock.Setup(x => x.GetListOfPrimeNumbers(totalOfNumbers)).Returns(listOfNumbers);
+        }
+
+        [Test]
+        public void then_a_list_of_numbers_is_returned()
+        {
+            // Arrange
+            var totalOfNumbersExpected = 5;
+            SetUpGetListOfPrimeNumbersMethod(totalOfNumbersExpected);
+
+            // Act
+            var listOfNumbersGenerated = base.RepositoryMock.Object.GetListOfPrimeNumbers(totalOfNumbersExpected);
+
+            // Assert
+            Assert.That(listOfNumbersGenerated, Is.Not.Null);
+            Assert.That(listOfNumbersGenerated.Length, Is.EqualTo(totalOfNumbersExpected));
+        }
+    }
+}
---
+++ PrimeNumbersTable.Web/Domain/Repositories/PrimeNumbersRepository.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.WebPages;
+
+namespace PrimeNumbersTable.Web.Domain.Repositories
+{
+    public interface IPrimeNumbersRepository
+    {
+        int[] GetListOfPrimeNumbers(int totalOfPrimeNumbers);
+    }
+
+    public class PrimeNumbersRepository
+    {
+        public int[] GetListOfPrimeNumbers(int totalOfPrimeNumbers)
+        {
+            // Here's where we're going to instantiate our prime numbers generation algorithm and make use of it
+            throw new NotImplementedException();
+        }
+    }
+}
