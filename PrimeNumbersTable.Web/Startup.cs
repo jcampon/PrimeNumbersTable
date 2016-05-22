b31b03c15a3d6e50d0664fed8c46537742a75fd9
Jesús Campón <jesus.campon@gmail.com>
2016-05-22 14:09:44 +0100

    First commit with initial web project skeleton and a few additional packages (NUnit and Moq for tests, StructureMap for dependency inversion and IoC implementation)

---
+++ PrimeNumbersTable.Web/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(PrimeNumbersTable.Web.Startup))]
+namespace PrimeNumbersTable.Web
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
