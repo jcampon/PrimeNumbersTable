e200d607c995a0195975ef3c79535bcc00eb5113
Jesús Campón <jesus.campon@gmail.com>
2016-05-23 02:17:44 +0100

    Added an additional test that would verify the validity of the prime-generating algorithm based on the first 500 known prime numbers

--- PrimeNumbersTable.Web/Utilities/BasicPrimeNumbersListGeneratorAlgorithm.cs
+++ PrimeNumbersTable.Web/Utilities/BasicPrimeNumbersListGeneratorAlgorithm.cs
@@ -4,9 +4,36 @@ namespace PrimeNumbersTable.Web.Utilities
 {
     public class BasicPrimeNumbersListGeneratorAlgorithm : IPrimeNumbersListGeneratorAlgorithm
     {
-        public int[] GetListOfFirstNPrimeNumbers(int numberOfPrimeNumbers)
+        public int[] GetListOfFirstNPrimeNumbers(int firsNPrimeNumbers)
         {
-            return new int[numberOfPrimeNumbers];
+            var listOfFirstNPrimeNumbers = new int[firsNPrimeNumbers];
+
+            // Special handling for the integer 2 which is the first prime
+            var countOfPrimesFoundAlready = 0;
+            listOfFirstNPrimeNumbers[countOfPrimesFoundAlready++] = 2;
+
+            // Looping from 3, to the limit
+            for (var number = 3; countOfPrimesFoundAlready < firsNPrimeNumbers; number++)
+            {
+                var isPrime = true;
+
+                for (var i = 0; (number / listOfFirstNPrimeNumbers[i]) >= listOfFirstNPrimeNumbers[i]; i++)
+                {
+                    if (number % listOfFirstNPrimeNumbers[i] == 0)
+                    {
+                        isPrime = false;
+                        break;
+                    }
+                }
+
+                // Store the prime number and increment the count
+                if (isPrime)
+                {
+                    listOfFirstNPrimeNumbers[countOfPrimesFoundAlready++] = number;
+                }
+            }
+
+            return listOfFirstNPrimeNumbers;
         }
     }
 }
