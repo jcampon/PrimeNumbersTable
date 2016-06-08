2d5da2e9987bb9228387b1b0421662cb14d6d541
Jesús Campón <jesus.campon@gmail.com>
2016-06-09 00:58:52 +0100

    Yet more renaming

--- PrimeNumbersTable.Web/Models/UserInputDisplayModel.cs
+++ PrimeNumbersTable.Web/Models/UserInputDisplayModel.cs
@@ -10,8 +10,8 @@ namespace PrimeNumbersTable.Web.Models
     public class UserInputDisplayModel
     {
         [Required(ErrorMessage = "You must provide an valid whole number between 1 and 500" )]
-        [DisplayName("Number of Prime Numbers to display")]
+        [DisplayName("Numbers to display")]
         [Range(1, 500)]
-        public int TotalOfPrimeNumbers { get; set; }
+        public int TotalOfNumbers { get; set; }
     }
 }
