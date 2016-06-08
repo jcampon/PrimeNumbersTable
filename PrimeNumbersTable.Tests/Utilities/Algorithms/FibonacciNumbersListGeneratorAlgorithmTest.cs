514e057da858da87c36ebc0f43766c3e13577d73
Jesús Campón <jesus.campon@gmail.com>
2016-06-08 20:57:53 +0100

    We had already introduced a bit of code duplication when creating the instance of the algorithm in both tests, so we fix it now by declaring the algorithm as a private field, initializing it on the test setup and removing its creation from the individual tests

--- PrimeNumbersTable.Tests/Utilities/Algorithms/FibonacciNumbersListGeneratorAlgorithmTest.cs
+++ PrimeNumbersTable.Tests/Utilities/Algorithms/FibonacciNumbersListGeneratorAlgorithmTest.cs
@@ -6,24 +6,26 @@ namespace PrimeNumbersTable.Tests.Utilities.Algorithms
     [TestFixture]
     public class when_working_with_the_fibonacci_numbers_list_generator_algorithm
     {
+        private IFibonacciNumbersListGeneratorAlgorithm _algorithm;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _algorithm = new FibonacciNumbersListGeneratorAlgorithm();
+        }
+
         [Test]
         public void test_that_the_algorithm_instance_implements_the_IFibonacciNumbersListGeneratorAlgorithm_interface()
         {
-            // Arrange
-            var algorithm = new FibonacciNumbersListGeneratorAlgorithm();
-
             // Assert
-            Assert.IsInstanceOf<IFibonacciNumbersListGeneratorAlgorithm>(algorithm);
+            Assert.IsInstanceOf<IFibonacciNumbersListGeneratorAlgorithm>(_algorithm);
         }
 
         [Test]
         public void test_that_the_GetListOfFirstNFibonacciNumbers_method_returns_an_array_of_int()
         {
-            // Arrange
-            var algorithm = new FibonacciNumbersListGeneratorAlgorithm();
-
             // Act
-            var fibonacciNumbers = algorithm.GetListOfFirstNFibonacciNumbers(5);
+            var fibonacciNumbers = _algorithm.GetListOfFirstNFibonacciNumbers(5);
 
             // Assert
             Assert.That(fibonacciNumbers, Is.InstanceOf(typeof(int[])));
