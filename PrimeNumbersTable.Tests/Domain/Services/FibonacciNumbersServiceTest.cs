9c1245f6b841a99697922685ac138491976f2a15
Jesús Campón <jesus.campon@gmail.com>
2016-06-08 23:53:29 +0100

    Now into testing our FibonacciNumbersService. We create a test class for it and start setting up a mock for the service, but the test will fail as we need to add the relevant interfaces

---
+++ PrimeNumbersTable.Tests/Domain/Services/FibonacciNumbersServiceTest.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Web.Mvc;
+using Moq;
+using NUnit.Framework;
+using PrimeNumbersTable.Web;
+using PrimeNumbersTable.Web.Domain.Services;
+
+namespace PrimeNumbersTable.Tests.Domain.Services
+{
+    [TestFixture]
+    public class when_working_with_the_FibonacciNumbersService
+    {
+        protected Mock<IFibonacciNumbersService> ServiceMock;
+
+        [SetUp]
+        protected void SetUpService()
+        {
+            this.ServiceMock = new Mock<IFibonacciNumbersService>();
+        }
+    }
+}
