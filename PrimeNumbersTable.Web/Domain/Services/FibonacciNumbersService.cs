cc182797e4acfbd4b3dd705e225ff17290b4f7b3
Jesús Campón <jesus.campon@gmail.com>
2016-06-08 23:55:57 +0100

    We add now the interface and the test setup now builds fine

---
+++ PrimeNumbersTable.Web/Domain/Services/FibonacciNumbersService.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using PrimeNumbersTable.Web.Utilities;
+using PrimeNumbersTable.Web.Utilities.Algorithms;
+
+namespace PrimeNumbersTable.Web.Domain.Services
+{
+    public interface IFibonacciNumbersService
+    {
+
+    }
+}
