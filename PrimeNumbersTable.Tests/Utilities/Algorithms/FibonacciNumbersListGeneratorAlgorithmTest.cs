76f111109879b756648c22b21007ed84a4148dfd
Jesús Campón <jesus.campon@gmail.com>
2016-06-08 23:34:05 +0100

    Refactor code to move the algorithm class and interface to their own file and to the web project

--- PrimeNumbersTable.Tests/Utilities/Algorithms/FibonacciNumbersListGeneratorAlgorithmTest.cs
+++ PrimeNumbersTable.Tests/Utilities/Algorithms/FibonacciNumbersListGeneratorAlgorithmTest.cs
@@ -69,44 +69,4 @@ namespace PrimeNumbersTable.Tests.Utilities.Algorithms
                                                                           fibonacciNumbers[arrayItemToValidate - 2]));
         }
     }
-
-    public interface IFibonacciNumbersListGeneratorAlgorithm
-    {
-        int[] GetListOfFirstNFibonacciNumbers(int totalOfNumbersExpected);
-    }
-
-    public class FibonacciNumbersListGeneratorAlgorithm : IFibonacciNumbersListGeneratorAlgorithm
-    {
-        private int[] _listOfFibonacciNumbers;
-
-        public int[] GetListOfFirstNFibonacciNumbers(int totalOfNumbersExpected)
-        {
-            InitializeListOfFibonacciNumbers(totalOfNumbersExpected);
-
-            for (var positionOnTheList = 2; positionOnTheList < totalOfNumbersExpected; positionOnTheList++)
-            {
-                CalculateValueFor(positionOnTheList);
-            }
-
-            return _listOfFibonacciNumbers;
-        }
-
-        private void CalculateValueFor(int positionOnTheList)
-        {
-            _listOfFibonacciNumbers[positionOnTheList] =
-                _listOfFibonacciNumbers[positionOnTheList - 1] + _listOfFibonacciNumbers[positionOnTheList - 2];
-        }
-
-        private void InitializeListOfFibonacciNumbers(int totalOfNumbersExpected)
-        {
-            _listOfFibonacciNumbers = new int[totalOfNumbersExpected];
-
-            _listOfFibonacciNumbers[0] = 1;
-
-            if (totalOfNumbersExpected >= 2)
-                _listOfFibonacciNumbers[1] = 1;
-        }
-
-    }
-
 }
