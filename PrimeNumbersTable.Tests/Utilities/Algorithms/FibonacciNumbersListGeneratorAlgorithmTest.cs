3195b42de7263fc6460c58e708c3a63ec2b0d2c3
jesus.campon <jesus.campon@gmail.com>
2016-06-08 15:58:48 +0100

    ** Initial commit of the Fibonacci Numbers generator extension to this test. We begin with adding a test class for it and an initial test method that will test if our new algorithm implements its appropriate interface. Currently the test does nothing yet

---
+++ PrimeNumbersTable.Tests/Utilities/Algorithms/FibonacciNumbersListGeneratorAlgorithmTest.cs
@@ -0,0 +1,15 @@
+﻿using NUnit.Framework;
+using PrimeNumbersTable.Web.Utilities.Algorithms;
+
+namespace PrimeNumbersTable.Tests.Utilities.Algorithms
+{
+    [TestFixture]
+    public class when_working_with_the_fibonacci_numbers_list_generator_algorithm
+    {
+        [Test]
+        public void test_that_the_algorithm_instance_implements_the_IFibonacciNumbersListGeneratorAlgorithm_interface()
+        {
+
+        }
+    }
+}
