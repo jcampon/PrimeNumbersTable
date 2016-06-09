1b11b53ec310843d360f21357faa51b598c9e16b
Jesús Campón <jesus.campon@gmail.com>
2016-06-09 01:49:28 +0100

    To finish with the test, let's remove some of the multiplication logic away from the view itself. In order to do this, I'll be moving this logic to the viewModel class by now. So I'm adding a test for this first of all. the code won't compile so I need to implement it next

---
+++ PrimeNumbersTable.Tests/Models/NumbersTableDisplayModelTest.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace PrimeNumbersTable.Tests.Models
+{
+    class when_working_with_the_NumbersTableDisplayModel
+    {
+        [Test]
+        public void test_that_the_GetValueToRenderInTableCell_returns_a_product_of_two_numbers()
+        {
+            // Assert
+            Assert.That(numberOne * numberTwo, Is.EqualTo(expectedProductOfNumbers));
+        }
+    }
+}
