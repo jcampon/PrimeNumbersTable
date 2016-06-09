b3b5908613653cf130946fbbb2db2681c5628f8e
Jesús Campón <jesus.campon@gmail.com>
2016-06-09 01:57:47 +0100

    The test won't compile yet but it's a bit clearer on what I want to get out of it

--- PrimeNumbersTable.Tests/Models/NumbersTableDisplayModelTest.cs
+++ PrimeNumbersTable.Tests/Models/NumbersTableDisplayModelTest.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using NUnit.Framework;
+using PrimeNumbersTable.Web.Models;
 
 namespace PrimeNumbersTable.Tests.Models
 {
@@ -12,8 +13,15 @@ namespace PrimeNumbersTable.Tests.Models
         [Test]
         public void test_that_the_GetValueToRenderInTableCell_returns_a_product_of_two_numbers()
         {
+            // Arrange
+            var viewModel = new NumbersTableDisplayModel(5);
+            viewModel.ListOfNumbers = new int[] {1, 2, 3, 4, 5};
+
+            // Act
+            actualProductOfNumbers = viewModel.GetValueToRenderInTableCell(1, 4); 
+
             // Assert
-            Assert.That(numberOne * numberTwo, Is.EqualTo(expectedProductOfNumbers));
+            Assert.That(actualProductOfNumbers, Is.EqualTo(10)); // We should get 2 * 5 = 10
         }
     }
 }
